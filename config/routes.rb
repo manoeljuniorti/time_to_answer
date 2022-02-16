e7018752463400b832923478a64db3fff6ec243c
manoeljuniorti <manoeljuniordev@gmail.com>
2022-02-16 12:44:14 -0300

    Instalando e configurando o Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
+ 
+  root to: 'welcome#index'
 end
