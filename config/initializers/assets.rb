3620d060dfc034e7fa9621e2cfdb3fc7d43ec49b
manoeljuniorti <manoeljuniordev@gmail.com>
2022-02-18 10:41:11 -0300

    Portando o tema Gentelella para User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( admin_backoffice.js admin_backoffice.css )
+Rails.application.config.assets.precompile += %w( user_backoffice.js user_backoffice.css )
