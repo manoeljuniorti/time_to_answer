ce5d47f39bbf4010699acc4096ade40ac83be2a4
manoeljuniorti <manoeljuniordev@gmail.com>
2022-03-09 14:58:51 -0300

    Usando Scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,13 +6,14 @@ class Question < ApplicationRecord
   #Kaminari
   paginates_per 5
 
-  def self.search(page, term)
-    Question.includes(:answers)
-            .where("lower(description) LIKE ?", "%#{term.downcase}%")
-            .page(page)
-  end
+  #Scopes (Utilizado para fazer querys no Banco de Dados)
+  scope :search, ->(page, term){
+    includes(:answers)
+    .where("lower(description) LIKE ?", "%#{term.downcase}%")
+    .page(page)
+  }
 
-  def self.last_questions(page)
-    Question.includes(:answers).order('created_at desc').page(page)
-  end
+  scope :last_questions, ->(page){
+    includes(:answers).order('created_at desc').page(page)
+  }
 end
